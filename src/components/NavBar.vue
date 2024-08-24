432e146d8b90c34ef32d62b7a6d9e7416f3de753
Calvin Yuen <scyuenab@gmail.com>
2024-08-24 11:53:04 +0800

    fix: Reset Page, Ues Tailwind, Use PrimeVue

---
+++ src/components/NavBar.vue
@@ -0,0 +1,3 @@
+<template>
+  <span> NavBar </span>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import './assets/main.css'
 
+import Aura from '@primevue/themes/aura'
+import PrimeVue from 'primevue/config'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -7,5 +9,9 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
-
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+})
 app.mount('#app')
